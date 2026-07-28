067bf0449a82134557824f88e507fbf1b53add13
Grace Chan <velviagris@outlook.com>
2026-07-28 09:09:42 +0800

    Bump version to 1.0.2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.gracethings.bubblenotice"
         minSdk = 30
         targetSdk = 37
-        versionCode = 11
-        versionName = "1.0.1"
+        versionCode = 12
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
